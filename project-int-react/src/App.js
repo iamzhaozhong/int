2f9b9bd521c897e939b1c9372b7ef496fc984740
Zhao Zhong <zhzhong@sas.upenn.edu>
2021-12-08 19:54:03 -0700

    added MUI

--- project-int-react/src/App.js
+++ project-int-react/src/App.js
@@ -7,31 +7,54 @@ import Navigation from './components/Navigation';
 import IndividualProduct from './components/InividualProduct';
 import ProductLink from './components/ProductLink';
 import SuggestProduct from "./components/SuggestProduct";
+import HeroSection from './pages/HeroSection';
 
 function App() {
-    const [allProducts, setAllProducts] = useState([]);
-    const [toggle, setToggle] = useState(false);
+	const [allProducts, setAllProducts] = useState([]);
+	const [toggle, setToggle] = useState(false);
 
-    useEffect(() => {
-        const getAllProducts = async () => {
-            const response = await getProduct();
-            setAllProducts(response);
-        };
-        getAllProducts();
-    }, [toggle]);
+	useEffect(() => {
+		const getAllProducts = async () => {
+			const response = await getProduct();
+			setAllProducts(response);
+		};
+		getAllProducts();
+	}, [toggle]);
 
-    return (
-        <div className="App">
-            <Navigation />
-            <br />
-            <Routes>
-                <Route path="/" element={<ProductLink products={allProducts} />} />
-                <Route path="/new" element={<SuggestProduct setToggle={setToggle}/>} />
-                <Route path="/edit/:id" element={<SuggestProduct products={allProducts} setToggle={setToggle}/>} />
-                <Route path="/products/:id" element={<IndividualProduct products={allProducts} setToggle={setToggle} />} />
-            </Routes>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Navigation />
+			<HeroSection />
+			<Routes>
+				<Route
+					path='/'
+					element={<ProductLink products={allProducts} />}
+				/>
+				<Route
+					path='/new'
+					element={<SuggestProduct setToggle={setToggle} />}
+				/>
+				<Route
+					path='/edit/:id'
+					element={
+						<SuggestProduct
+							products={allProducts}
+							setToggle={setToggle}
+						/>
+					}
+				/>
+				<Route
+					path='/products/:id'
+					element={
+						<IndividualProduct
+							products={allProducts}
+							setToggle={setToggle}
+						/>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
